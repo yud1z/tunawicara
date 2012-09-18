a94b04e114239b76ae063d3cf625a0ff4fb06a15
yud1z <yud1z.dev@gmail.com>
2012-09-18 21:20:54 +0700

    first commit
    Signed-off-by: yud1z <yud1z.dev@gmail.com>

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bukaFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simpanFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tampilanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.warnaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alignmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kananToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tengahToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kiriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.tampilanToolStripMenuItem,
+            this.formatToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bukaFileToolStripMenuItem,
+            this.simpanFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // bukaFileToolStripMenuItem
+            // 
+            this.bukaFileToolStripMenuItem.Name = "bukaFileToolStripMenuItem";
+            this.bukaFileToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.bukaFileToolStripMenuItem.Text = "Buka File";
+            this.bukaFileToolStripMenuItem.Click += new System.EventHandler(this.bukaFileToolStripMenuItem_Click);
+            // 
+            // simpanFileToolStripMenuItem
+            // 
+            this.simpanFileToolStripMenuItem.Name = "simpanFileToolStripMenuItem";
+            this.simpanFileToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.simpanFileToolStripMenuItem.Text = "Simpan File";
+            this.simpanFileToolStripMenuItem.Click += new System.EventHandler(this.simpanFileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tampilanToolStripMenuItem
+            // 
+            this.tampilanToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoomInToolStripMenuItem,
+            this.zoomOutToolStripMenuItem});
+            this.tampilanToolStripMenuItem.Name = "tampilanToolStripMenuItem";
+            this.tampilanToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.tampilanToolStripMenuItem.Text = "Tampilan";
+            // 
+            // zoomInToolStripMenuItem
+            // 
+            this.zoomInToolStripMenuItem.Name = "zoomInToolStripMenuItem";
+            this.zoomInToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.zoomInToolStripMenuItem.Text = "Zoom in +";
+            this.zoomInToolStripMenuItem.Click += new System.EventHandler(this.zoomInToolStripMenuItem_Click);
+            // 
+            // zoomOutToolStripMenuItem
+            // 
+            this.zoomOutToolStripMenuItem.Name = "zoomOutToolStripMenuItem";
+            this.zoomOutToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.zoomOutToolStripMenuItem.Text = "Zoom out -";
+            this.zoomOutToolStripMenuItem.Click += new System.EventHandler(this.zoomOutToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.warnaToolStripMenuItem,
+            this.fontToolStripMenuItem,
+            this.alignmentToolStripMenuItem});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // warnaToolStripMenuItem
+            // 
+            this.warnaToolStripMenuItem.Name = "warnaToolStripMenuItem";
+            this.warnaToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.warnaToolStripMenuItem.Text = "Warna";
+            this.warnaToolStripMenuItem.Click += new System.EventHandler(this.warnaToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // alignmentToolStripMenuItem
+            // 
+            this.alignmentToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kananToolStripMenuItem,
+            this.tengahToolStripMenuItem,
+            this.kiriToolStripMenuItem});
+            this.alignmentToolStripMenuItem.Name = "alignmentToolStripMenuItem";
+            this.alignmentToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.alignmentToolStripMenuItem.Text = "Alignment";
+            // 
+            // kananToolStripMenuItem
+            // 
+            this.kananToolStripMenuItem.Name = "kananToolStripMenuItem";
+            this.kananToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.kananToolStripMenuItem.Text = "Kanan";
+            this.kananToolStripMenuItem.Click += new System.EventHandler(this.kananToolStripMenuItem_Click);
+            // 
+            // tengahToolStripMenuItem
+            // 
+            this.tengahToolStripMenuItem.Name = "tengahToolStripMenuItem";
+            this.tengahToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.tengahToolStripMenuItem.Text = "Tengah";
+            this.tengahToolStripMenuItem.Click += new System.EventHandler(this.tengahToolStripMenuItem_Click);
+            // 
+            // kiriToolStripMenuItem
+            // 
+            this.kiriToolStripMenuItem.Name = "kiriToolStripMenuItem";
+            this.kiriToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.kiriToolStripMenuItem.Text = "Kiri";
+            this.kiriToolStripMenuItem.Click += new System.EventHandler(this.kiriToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(292, 249);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Ketik Disini";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "rtf";
+            this.saveFileDialog1.Filter = "rtf file | *rtf";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Baca";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 227);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 46);
+            this.panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Testing yudis";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bukaFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simpanFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tampilanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem warnaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alignmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kananToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tengahToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kiriToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem zoomInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomOutToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SpeechLib;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        SpVoice objekspeech = new SpVoice();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void kananToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void tengahToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void kiriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void zoomInToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.ZoomFactor += 2f;
+        }
+
+        private void zoomOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.ZoomFactor > 1)
+            {
+                richTextBox1.ZoomFactor -= 2f;
+            }
+            
+        }
+
+        private void warnaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK )
+            {
+                richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+            
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK )
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+            }
+            
+        }
+
+        private void bukaFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void simpanFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string namafile;
+            namafile = openFileDialog1.FileName;
+            //sMessageBox.Show(namafile);
+
+            if (namafile != "")
+            {
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    namafile = saveFileDialog1.FileName;
+                    richTextBox1.SaveFile(namafile);
+                }
+
+            }
+            else
+            {
+                richTextBox1.SaveFile(namafile);
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            objekspeech.Speak(richTextBox1.Text.Trim(), SpeechVoiceSpeakFlags.SVSFDefault);
+            objekspeech.WaitUntilDone(10);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            objekspeech.Pause();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
