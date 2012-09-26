ad8c276946ae1512831272599f815b0c1aa48bab
yud1z <yud1z.dev@gmail.com>
2012-09-26 19:38:52 +0700

    translate support
    Signed-off-by: yud1z <yud1z.dev@gmail.com>

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -51,6 +51,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -63,7 +66,7 @@
             this.formatToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(661, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -74,27 +77,27 @@
             this.simpanFileToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // bukaFileToolStripMenuItem
             // 
             this.bukaFileToolStripMenuItem.Name = "bukaFileToolStripMenuItem";
-            this.bukaFileToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.bukaFileToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.bukaFileToolStripMenuItem.Text = "Buka File";
             this.bukaFileToolStripMenuItem.Click += new System.EventHandler(this.bukaFileToolStripMenuItem_Click);
             // 
             // simpanFileToolStripMenuItem
             // 
             this.simpanFileToolStripMenuItem.Name = "simpanFileToolStripMenuItem";
-            this.simpanFileToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.simpanFileToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.simpanFileToolStripMenuItem.Text = "Simpan File";
             this.simpanFileToolStripMenuItem.Click += new System.EventHandler(this.simpanFileToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -104,20 +107,20 @@
             this.zoomInToolStripMenuItem,
             this.zoomOutToolStripMenuItem});
             this.tampilanToolStripMenuItem.Name = "tampilanToolStripMenuItem";
-            this.tampilanToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.tampilanToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
             this.tampilanToolStripMenuItem.Text = "Tampilan";
             // 
             // zoomInToolStripMenuItem
             // 
             this.zoomInToolStripMenuItem.Name = "zoomInToolStripMenuItem";
-            this.zoomInToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.zoomInToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.zoomInToolStripMenuItem.Text = "Zoom in +";
             this.zoomInToolStripMenuItem.Click += new System.EventHandler(this.zoomInToolStripMenuItem_Click);
             // 
             // zoomOutToolStripMenuItem
             // 
             this.zoomOutToolStripMenuItem.Name = "zoomOutToolStripMenuItem";
-            this.zoomOutToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.zoomOutToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.zoomOutToolStripMenuItem.Text = "Zoom out -";
             this.zoomOutToolStripMenuItem.Click += new System.EventHandler(this.zoomOutToolStripMenuItem_Click);
             // 
@@ -128,20 +131,20 @@
             this.fontToolStripMenuItem,
             this.alignmentToolStripMenuItem});
             this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
-            this.formatToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
             this.formatToolStripMenuItem.Text = "Format";
             // 
             // warnaToolStripMenuItem
             // 
             this.warnaToolStripMenuItem.Name = "warnaToolStripMenuItem";
-            this.warnaToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.warnaToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.warnaToolStripMenuItem.Text = "Warna";
             this.warnaToolStripMenuItem.Click += new System.EventHandler(this.warnaToolStripMenuItem_Click);
             // 
             // fontToolStripMenuItem
             // 
             this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
-            this.fontToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.fontToolStripMenuItem.Text = "Font";
             this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
             // 
@@ -152,27 +155,27 @@
             this.tengahToolStripMenuItem,
             this.kiriToolStripMenuItem});
             this.alignmentToolStripMenuItem.Name = "alignmentToolStripMenuItem";
-            this.alignmentToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.alignmentToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.alignmentToolStripMenuItem.Text = "Alignment";
             // 
             // kananToolStripMenuItem
             // 
             this.kananToolStripMenuItem.Name = "kananToolStripMenuItem";
-            this.kananToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.kananToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.kananToolStripMenuItem.Text = "Kanan";
             this.kananToolStripMenuItem.Click += new System.EventHandler(this.kananToolStripMenuItem_Click);
             // 
             // tengahToolStripMenuItem
             // 
             this.tengahToolStripMenuItem.Name = "tengahToolStripMenuItem";
-            this.tengahToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.tengahToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.tengahToolStripMenuItem.Text = "Tengah";
             this.tengahToolStripMenuItem.Click += new System.EventHandler(this.tengahToolStripMenuItem_Click);
             // 
             // kiriToolStripMenuItem
             // 
             this.kiriToolStripMenuItem.Name = "kiriToolStripMenuItem";
-            this.kiriToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.kiriToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.kiriToolStripMenuItem.Text = "Kiri";
             this.kiriToolStripMenuItem.Click += new System.EventHandler(this.kiriToolStripMenuItem_Click);
             // 
@@ -181,7 +184,7 @@
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richTextBox1.Location = new System.Drawing.Point(0, 24);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(292, 249);
+            this.richTextBox1.Size = new System.Drawing.Size(661, 376);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "Ketik Disini";
             // 
@@ -206,17 +209,20 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 227);
+            this.panel1.Location = new System.Drawing.Point(0, 354);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(292, 46);
+            this.panel1.Size = new System.Drawing.Size(661, 46);
             this.panel1.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(180, 11);
+            this.button2.Location = new System.Drawing.Point(105, 11);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -224,11 +230,36 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(555, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Translate";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(255, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(404, 13);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(661, 400);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
@@ -269,6 +300,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
